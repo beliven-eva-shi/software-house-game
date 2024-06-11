eb34783aa8d1a93e9587839103fb4c28fd201fc4
Eva Shi <eva.shi@beliven.com>
2024-06-11 17:58:31 +0200

    feat: sales view

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -1,9 +1,17 @@
+<meta http-equiv="refresh" content="1;url=/sales" />
 <x-layout>
 
     <body>
+        <h1 style="font-size: 3em; font-weight: bold; text-align: center;">Sales</h1>
         <div style="display: flex; height: 100vh; padding: 20px;">
             <x-column>
-                <x-card>Commerciali</x-card>
+                @foreach ($sales as $sale)
+                    <x-card>
+                        <div>{{ $sale->name }} </div>
+                        <div>Seniority Lv: {{ $sale->seniority_lv }}</div>
+                        <div>Next project in: {{ $sale->time_new_project }}s </div>
+                    </x-card>
+                @endforeach
             </x-column>
         </div>
     </body>
