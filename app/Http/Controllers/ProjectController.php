8b0791f7f73ffca33c98e182360515c2e347e19b
Eva Shi <eva.shi@beliven.com>
2024-06-12 11:59:30 +0200

    fix: assigning project change state to dev

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,7 +15,8 @@ class ProjectController extends Controller
         $project->dev_id = $request->input('assign');
         $dev = Developer::findOrFail($request->input('assign'));
         $project->time_for_completion = ceil($project->complexity / $dev->seniority_lv);
-
+        $dev->available_flg = 0;
+        $dev->save();
         $project->save();
 
         return redirect("/production")->with('success', 'Project assigned!');
