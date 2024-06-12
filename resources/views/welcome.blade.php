cfe6560b6b4a54c69b665254c6f171137d866ec8
Eva Shi <eva.shi@beliven.com>
2024-06-12 11:58:24 +0200

    fix: welcome page fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<x-layout>
 
-    <head>
-        <style>
-            .container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                flex-direction: column;
-            }
+<head>
+    <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+        }
 
-            .button {
-                background-color: #00aaff;
-                color: white;
-                padding: 15px 30px;
-                margin: 10px;
-                border: none;
-                border-radius: 5px;
-                text-align: center;
-                text-decoration: none;
-                font-size: 16px;
-                transition: background-color 0.3s ease;
-            }
+        .button {
+            background-color: #00aaff;
+            color: white;
+            padding: 15px 30px;
+            margin: 10px;
+            border: none;
+            border-radius: 5px;
+            text-align: center;
+            text-decoration: none;
+            font-size: 16px;
+            font-family: Arial;
+            transition: background-color 0.3s ease;
+        }
 
-            .button:hover {
-                background-color: #0088cc;
-            }
-        </style>
-    </head>
+        .button:hover {
+            background-color: #0088cc;
+        }
+    </style>
+</head>
 
-    <body>
-        <div class="container">
-            <a href="/production" class="button">Resume</a>
-            <a href="#" class="button">New game session</a>
-        </div>
-    </body>
-</x-layout>
+<body>
+    <div class="container">
+        <a href="/production" class="button">Resume</a>
+        <a href="#" class="button">New game session</a>
+    </div>
+</body>
+
 
 </html>
